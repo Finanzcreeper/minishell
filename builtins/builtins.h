425b747b4ab9b34abede7ea7c713664777c62272
Graham Booth <graham.r.booth@gmail.com>
2023-05-31 12:30:03 +0200

    refactored builtins - still needs better testing and fixing of norm errors

--- builtins/builtins.h
+++ builtins/builtins.h
@@ -5,15 +5,12 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <ctype.h>
+#include "libft.h"
 
-char **remove_key_from_envp(char **envp, char* key_to_remove);
-void builtin_echo(int num_args, char** args);
-int builtin_cd(int num_args, char** args);
-int builtin_pwd(int num_args);
-char *get_key_from_line(char *line);
-int builtin_export(int num_args, char** args, char **envp);
-int builtin_unset(int num_args, char** args, char **envp);
-int builtin_env(int num_args, char **envp);
-int builtin_exit(int num_args, char arg[]);
+int	builtin_cd(int num_args, char **args);
+void	builtin_echo(int num_args, char **args);
+int	builtin_export(int num_args, char **args, char **envp);
+int	builtin_unset(char **args, char **envp);
+char	**remove_key_from_envp(char **envp, char *key_to_remove);
+int builtin_pwd();
 void run_builtin(char *command, int num_args, char *args[], char **envp);
-// int main(int argc, char **argv, char **envp);
---
+++ builtins/builtins1.c
@@ -0,0 +1,120 @@
+#include "builtins.h"
+
+// extract key from envp line (all chars before first equals sign)
+// used by 'export' and 'unset'
+// approach here works but maybe better to
+// allocate a new block of memory and copy across
+char	**remove_key_from_envp(char **envp, char *key_to_remove)
+{
+	int		j;
+	bool	bunch_up;
+	char	*from_equals;
+	int		line_key_len;
+	char	*line_key;
+
+	j = 0;
+	bunch_up = false;
+	while (envp[j])
+	{	
+		from_equals = ft_strchr(envp[j], '=');
+		line_key_len = ft_strlen(envp[j]) - ft_strlen(from_equals);
+		line_key = ft_substr(envp[j], 0, line_key_len);
+		if (ft_strncmp(key_to_remove, line_key, ft_strlen(key_to_remove)) == 0)
+			bunch_up = true;
+		if (bunch_up == true)
+		{
+			if (envp[j + 1])
+				envp[j] = envp[j + 1];
+			else
+				envp[j] = NULL;
+		}
+		j++;
+	}
+	return (envp);
+}
+
+// echo (with option -n)
+// display a line of text passed as an argument
+void	builtin_echo(int num_args, char **args)
+{
+	bool	print_newline;
+	int		i;
+
+	if (num_args == 0)
+	{
+		printf("\n");
+		return ;
+	}
+	print_newline = true;
+	if (ft_strncmp(args[0], "-n", ft_strlen(args[0])) == 0)
+	{
+		print_newline = false;
+		args++;
+	}
+	i = 0;
+	while (args[i])
+	{
+		if (i != 0)
+			printf(" ");
+		printf("%s", args[i]);
+		i++;
+	}
+	if (print_newline)
+		printf("\n");
+}
+
+// cd (with only a relative or absolute path)
+// change current working directory
+int	builtin_cd_absolute(char *path)
+{
+	int	status;
+
+	status = chdir(path);
+	if (status != 0)
+	{
+		printf("bash: cd: %s: No such file or directory\n", path);
+		return (1);
+	}
+	return (0);
+}
+
+// concats buf_cwd, slash and path
+int	builtin_cd_relative(char *path)
+{
+	char	buf_cwd[PATH_MAX];
+	int		status;
+
+	if (getcwd(buf_cwd, sizeof(buf_cwd)) != NULL)
+	{
+		char relative_path[ft_strlen(buf_cwd) + ft_strlen(path) + 2];
+		ft_strlcpy(relative_path, buf_cwd, ft_strlen(relative_path));
+		ft_strlcat(relative_path, "/", 1);
+		ft_strlcat(relative_path, path, ft_strlen(path));
+		status = chdir(path);
+		if (status != 0)
+		{
+			printf("bash: cd: %s: No such file or directory\n", path);
+			return (1);
+		}
+		return (0);
+	}
+	perror("getcwd() error");
+	return (1);
+}
+
+int	builtin_cd(int num_args, char **args)
+{
+	char	*path;
+
+	if (num_args == 0)
+		return (1);
+	if (num_args > 1)
+	{
+		printf("cd: too many arguments\n");
+		return (1);
+	}
+	path = args[0];
+	if (path[0] == '/')
+		return (builtin_cd_absolute(path));
+	return (builtin_cd_relative(path));
+}
